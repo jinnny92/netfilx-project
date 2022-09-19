99795c41700b1e54c7a92bbca6a3575e0bf92d4a
jinnny92 <jinnny22@naver.com>
2022-09-20 00:45:38 +0900

    리덕스기본세팅

---
+++ netfilx-project/src/redux/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducer";
+
+// let store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
+
+const store = configureStore({
+  reducer: {},
+});
+
+export default store;
